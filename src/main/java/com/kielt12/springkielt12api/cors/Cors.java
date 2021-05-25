eb6f470fec71dc518f6538c638d6127023daface
kielt12 <47779993+kielt12@users.noreply.github.com>
2021-05-25 14:41:17 -0500

    update

---
+++ src/main/java/com/kielt12/springkielt12api/cors/Cors.java
@@ -0,0 +1,16 @@
+package com.kielt12.springkielt12api.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Cors implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "OPTIONS").allowedOrigins("*");
+    }
+}
+
